b54ba6ef0b6393b0c21a533d8a1a08a0c05e01b1
Ezequiel <ezgoca@gmail.com>
2020-05-22 09:52:54 -0300

    220520

--- exercicio32.py
+++ exercicio32.py
@@ -1,7 +1,7 @@
-ano = int(input('Digite o ano atual com 4 digitos: \n'))
-# if ano%4==0 and ano%100!=0 or ano%400==0:
-if ano%4==0:
-    if ano%100!=0 or ano%400==0:
+ano = int(input('Digite o ano a ser analisado com 4 digitos: \n'))
+if ano%4==0 and ano%100!=0 or ano%400==0:
+# if ano%4==0:
+#     if ano%100!=0 or ano%400==0:
             print('O ano {} digitado é bisexto'.format(ano))
 else:
     print('O ano {} digitado NÃO é bisexto'.format(ano))
